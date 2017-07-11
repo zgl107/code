00d1ed0f90f84f59d7b7101eb758ba1700ad62bd
zgl107 <zgl6498282>
2017-07-12 00:49:23 +0800

    新闻列表功能完成

--- js/main.js
+++ js/main.js
@@ -25,4 +25,40 @@
 
      };
  $(window).on('resize',resize).trigger('resize');
+
+
+ /**
+  * 控制标签页的容器宽度
+ */
+ var $ulContainer = $('.nav-tabs');
+     // 获取所有子元素的宽度和
+     var width = 30; // 因为原本ul上有padding-left
+     // 遍历子元素
+     $ulContainer.children().each(function(index, element) {
+         // console.log(element.clientWidth);
+         // console.log($(element).width());
+         width += element.clientWidth;
+     });
+     // 此时width等于所有LI的宽度总和
+     // 判断当前UL的宽度是否超出屏幕，如果超出就显示横向滚动条
+     if (width > $(window).width()) {
+
+         $ulContainer
+             .css('width', width)
+             .parent().css('overflow-x', 'scroll');
+     }
+
+/**
+ *  a点击注册事件
+  */
+    var $newsTitle = $('.news-title');
+$("#news .nav-pills a").on('click',function(){
+    //获取当前点击元素
+    var $this = $(this);
+    //获取对应的title值
+    var title = $this.data('title');
+    //将title值设置到相应的位置
+    $newsTitle.text(title);
+});
+
  });
